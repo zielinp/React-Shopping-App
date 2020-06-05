12c21bcaa3d16eec1790bc7d4df317501b70a027
Paulina Zielinska <pzielinska.zielona@gmail.com>
2020-06-05 19:55:47 +0200

    Updates

---
+++ precache-manifest.495274c9023aa23391148b7f04406981.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b11dfaa534933f5731c57b9ee0abec6",
+    "url": "/React-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "50c22298f172f0a3e948",
+    "url": "/React-Shopping-Cart/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0ac98bf59327390baad2",
+    "url": "/React-Shopping-Cart/static/css/main.f28cd18e.chunk.css"
+  },
+  {
+    "revision": "50c22298f172f0a3e948",
+    "url": "/React-Shopping-Cart/static/js/2.7878d7f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Shopping-Cart/static/js/2.7878d7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac98bf59327390baad2",
+    "url": "/React-Shopping-Cart/static/js/main.1d7f32f6.chunk.js"
+  },
+  {
+    "revision": "5e6471693f0049ffee89",
+    "url": "/React-Shopping-Cart/static/js/runtime-main.2cb32aab.js"
+  },
+  {
+    "revision": "db719225665bfc19dfc51aa78a794c30",
+    "url": "/React-Shopping-Cart/static/media/brand_logo.db719225.png"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/React-Shopping-Cart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/React-Shopping-Cart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/React-Shopping-Cart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/React-Shopping-Cart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/React-Shopping-Cart/static/media/item6.f5fd97b5.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Shopping-Cart/precache-manifest.495274c9023aa23391148b7f04406981.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Shopping-Cart/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
